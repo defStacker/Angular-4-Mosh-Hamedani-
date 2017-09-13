db606c02eea35814d2649e051bea06c9807ad16a
Denys Zorin <zorin.denis94@outlook.com>
2017-09-13 16:51:01 +0200

    contact-form

---
+++ src/app/contact-form/contact-form.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-form',
+  templateUrl: './contact-form.component.html',
+  styleUrls: ['./contact-form.component.css']
+})
+export class ContactFormComponent  {
+  contactMethods = [
+    { id: 1, name: 'Email' },
+    { id: 2, name: 'Phone' },
+  ];
+
+  submit(f) {
+    console.log(f);
+  }
+}
